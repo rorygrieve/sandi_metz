8020d8cc1ac76df201f0d1d4844231d55e6c893e
rory <rhrg500@gmail.com>
2017-02-14 14:21:21 +0000

    adds attr_reader to encapsulate instance variables

--- gear.rb
+++ gear.rb
@@ -1,12 +1,14 @@
 class Gear
 
+  attr_reader :chainrig, :cog
+
   def initialize(chaingrig, cog)
     @chainrig = chaingrig
     @cog      = cog
   end
 
   def ratio
-    @chainrig / @cog.to_f
+    chainrig / cog.to_f
   end
 
 end
