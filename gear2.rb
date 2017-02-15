d00c5c9cb382d27971c128cf9560604beabc9abf
rory <rhrg500@gmail.com>
2017-02-15 16:36:08 +0000

    removes argument order dependencies using an args hash

--- gear2.rb
+++ gear2.rb
@@ -1,9 +1,9 @@
 class Gear
   attr_reader :chainring, :cog, :wheel
-  def initialize(chainring, cog, wheel)
-    @chainring = chainring
-    @cog       = cog
-    @wheel     = wheel
+  def initialize(args)
+    @chainring = args[:chainring]
+    @cog       = args[:cog]
+    @wheel     = args[:wheel]
   end
 
   def gear_inches
@@ -33,4 +33,4 @@ class Wheel
 
 end
 
-Gear.new(52, 11, Wheel.new(26, 1.5)).gear_inches
+Gear.new(:chainring => 52, :cog => 11, :wheel => Wheel.new(26, 1.5)).gear_inches
