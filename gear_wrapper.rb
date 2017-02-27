31da8389fd94d9390cf2e9eb8ccca4fb0f624398
rory <rhrg500@gmail.com>
2017-02-27 16:38:25 +0000

    uses a module to wrap the class as you would if Gear was part of an external application

---
+++ gear_wrapper.rb
@@ -0,0 +1,50 @@
+# When Gear is part of an external interface
+module SomeFramework
+  class Gear
+    attr_reader :chainring, :cog, :wheel
+    def initialize(chainring, cog, wheel)
+      @chainring = chainring
+      @cog       = cog
+      @wheel     = wheel
+    end
+
+    def gear_inches
+      ratio * diameter
+    end
+
+    def diameter
+      wheel.diameter
+    end
+
+    def ratio
+      chainring / cog
+    end
+
+  end
+end
+
+class Wheel
+  attr_reader :rim, :tire
+  def initialize(rim, tire)
+    @rim  = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+end
+
+
+# Wrap the interfact to protect yourself from changes
+module GearWrapper
+  def self.gear(args)
+    SomeFramework::Gear.new(args[:chainring],
+                            args[:cog],
+                            args[:wheel])
+  end
+end
+
+# Now you can create a new Gear using an arguments hash
+GearWrapper.gear(:chainring => 52, :cog => 11, :wheel => Wheel.new(26, 1.5)).gear_inches
